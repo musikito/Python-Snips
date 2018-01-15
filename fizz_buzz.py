83630dc8df554affa9ef4521462fbd8fb3551c78
musikito <tumusicalatina@gmail.com>
2018-01-15 08:39:16 -0500

    Update fizz_buzz.py

--- fizz_buzz.py
+++ fizz_buzz.py
@@ -16,3 +16,25 @@ for num in range(1, 21):
     else:
         '''if is neither then just print the number'''
         print(num)
+        
+This is the result:        
+1
+2
+Fizz
+4
+Buzz
+Fizz
+7
+8
+Fizz
+Buzz
+11
+Fizz
+13
+14
+FizzBuzz
+16
+17
+Fizz
+19
+Buzz
